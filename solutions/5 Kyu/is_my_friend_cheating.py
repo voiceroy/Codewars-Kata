833c7f1fc8a572e426925ece8c39f70a80beffa8
Harishankar Gaddanakeri <harishankargaddanakeri@gmail.com>
2022-10-14 15:00:49 +0530

    optimize is_my_friend_cheating.py

--- solutions/5 Kyu/is_my_friend_cheating.py
+++ solutions/5 Kyu/is_my_friend_cheating.py
@@ -4,7 +4,7 @@ def remov_nb(n: int) -> list:
 
     for a in range(1, n + 1):
         b = (interval_sum - a) / (a + 1)
-        if b == int(b) and b < n:
+        if b.is_integer() and b < n:
             possibilities.append((a, int(b)))
 
     return possibilities
