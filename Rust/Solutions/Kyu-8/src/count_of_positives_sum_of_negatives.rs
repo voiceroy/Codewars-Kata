3d4942fefac041ebe40c5be31e7e90ada6b73be9
Harishankar Gaddanakeri <harishankargaddanakeri@gmail.com>
2022-10-20 16:03:33 +0530

    Improved count_of_positives_sum_of_negatives.rs

--- Rust/Solutions/Kyu-8/src/count_of_positives_sum_of_negatives.rs
+++ Rust/Solutions/Kyu-8/src/count_of_positives_sum_of_negatives.rs
@@ -1,13 +1,17 @@
+use std::cmp::Ordering;
+
 fn count_positives_sum_negatives(input: Vec<i32>) -> Vec<i32> {
-    if input.len() > 0 {
+    if !input.is_empty() {
         let mut count_positives = 0;
         let mut sum_negatives = 0;
 
         for number in &input {
-            if number > &0 {
-                count_positives += 1
-            } else if number < &0 {
-                sum_negatives += number
+            match number.cmp(&0) {
+                Ordering::Less => {
+                    sum_negatives += number;
+                }
+                Ordering::Greater => count_positives += 1,
+                Ordering::Equal => (), // Do nothing
             }
         }
 
