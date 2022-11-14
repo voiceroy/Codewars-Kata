bf025118df69eb2504e4e5192b73f908d08ed773
Harishankar G <harishankargaddanakeri@gmail.com>
2022-11-14 21:14:52 +0530

    opposites_attract.rs

--- Rust/Solutions/kyu_8/src/main.rs
+++ Rust/Solutions/kyu_8/src/main.rs
@@ -1,7 +1,7 @@
 mod count_of_positives_sum_of_negatives;
 mod fake_binary;
+mod opposites_attract;
 mod simple_multiplication;
 mod what_is_between;
 mod you_cant_code_under_pressure_1;
-
 fn main() {}
