814eee13486a17b4a4f0c55ad6cb6efb2d837740
Harishankar G <harishankargaddanakeri@gmail.com>
2022-11-14 22:19:16 +0530

    string_repeat.rs

--- Rust/Solutions/kyu_8/src/main.rs
+++ Rust/Solutions/kyu_8/src/main.rs
@@ -4,6 +4,7 @@ mod fake_binary;
 mod opposites_attract;
 mod parse_nice_int_from_char_problem;
 mod simple_multiplication;
+mod string_repeat;
 mod what_is_between;
 mod you_cant_code_under_pressure_1;
 fn main() {}
