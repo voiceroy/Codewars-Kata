6dcfbb43fcba15c8c10b3fb8fd40317a113ef4ff
Harishankar G <harishankargaddanakeri@gmail.com>
2022-11-14 22:31:43 +0530

    how_many_lightsabers_do_you_own.rs

--- Rust/Solutions/kyu_8/src/main.rs
+++ Rust/Solutions/kyu_8/src/main.rs
@@ -3,6 +3,7 @@ mod basic_mathematical_operations;
 mod convert_to_a_string_to_a_number;
 mod count_of_positives_sum_of_negatives;
 mod fake_binary;
+mod how_many_lightsabers_do_you_own;
 mod opposites_attract;
 mod parse_nice_int_from_char_problem;
 mod simple_multiplication;
