26ae914e10e8700974d614700d8adb1bd060fdad
Harishankar G <harishankargaddanakeri@gmail.com>
2022-11-14 22:35:04 +0530

    squaring_an_argument.rs

--- Rust/Solutions/kyu_8/src/main.rs
+++ Rust/Solutions/kyu_8/src/main.rs
@@ -7,6 +7,7 @@ mod how_many_lightsabers_do_you_own;
 mod opposites_attract;
 mod parse_nice_int_from_char_problem;
 mod simple_multiplication;
+mod squaring_an_argument;
 mod string_repeat;
 mod what_is_between;
 mod you_cant_code_under_pressure_1;
