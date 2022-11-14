6bb13a835997f5b6777a21e7a2ae07b8652c1f9e
Harishankar G <harishankargaddanakeri@gmail.com>
2022-11-14 21:32:34 +0530

    parse_nice_int_from_char_problem.rs

--- Rust/Solutions/kyu_8/src/main.rs
+++ Rust/Solutions/kyu_8/src/main.rs
@@ -1,6 +1,7 @@
 mod count_of_positives_sum_of_negatives;
 mod fake_binary;
 mod opposites_attract;
+mod parse_nice_int_from_char_problem;
 mod simple_multiplication;
 mod what_is_between;
 mod you_cant_code_under_pressure_1;
