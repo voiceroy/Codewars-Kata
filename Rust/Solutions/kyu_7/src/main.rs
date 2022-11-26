48968bb6e018bd1936d0bdcd543832f6111c2158
Harishankar G <harishankargaddanakeri@gmail.com>
2022-11-26 18:14:00 +0530

    sort_out_the_men_from_boys.rs

--- Rust/Solutions/kyu_7/src/main.rs
+++ Rust/Solutions/kyu_7/src/main.rs
@@ -7,6 +7,7 @@ mod nth_smallest_element;
 mod ordered_count_of_characters;
 mod power_of_two;
 mod sort_numbers;
+mod sort_out_the_men_from_boys;
 mod sum_of_a_sequence;
 mod sum_of_angles;
 mod sum_of_cubes;
