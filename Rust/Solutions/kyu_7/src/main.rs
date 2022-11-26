f5c753e4916f5a60093973f56c7978039ef9c765
Harishankar G <harishankargaddanakeri@gmail.com>
2022-11-26 16:36:24 +0530

    power_of_two.rs

--- Rust/Solutions/kyu_7/src/main.rs
+++ Rust/Solutions/kyu_7/src/main.rs
@@ -3,6 +3,7 @@ mod check_the_exam;
 mod deodorant_evaporator;
 mod fix_string_case;
 mod ordered_count_of_characters;
+mod power_of_two;
 mod sort_numbers;
 mod sum_of_a_sequence;
 mod sum_of_angles;
