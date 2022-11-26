89b85c1a097b26f4e41be06a1aedd5f08179bec2
Harishankar G <harishankargaddanakeri@gmail.com>
2022-11-26 15:46:39 +0530

    ordered_count_of_characters.rs

--- Rust/Solutions/kyu_7/src/main.rs
+++ Rust/Solutions/kyu_7/src/main.rs
@@ -2,6 +2,7 @@ mod alphabet_summary;
 mod check_the_exam;
 mod deodorant_evaporator;
 mod fix_string_case;
+mod ordered_count_of_characters;
 mod sum_of_a_sequence;
 mod sum_of_angles;
 mod sum_of_cubes;
